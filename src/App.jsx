e671ea4b6fc6f8b706862b9e34515b41fa3b6eb3
Sivaneshkkf <sivaneshkkf@gmail.com>
2024-11-27 22:43:39 +0530

    fix padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Form from "./Components/Form";
 
 const App = () => {
   return (
-    <div className="w-full flex items-center justify-center h-screen overflow-hidden bg-gradient-to-br from-primary to-secondary">
+    <div className="w-full flex items-center justify-center h-screen overflow-hidden bg-gradient-to-br from-primary to-secondary p-5">
       <div className="w-full mx-auto max-w-xl rounded-xl bg-white shadow-lg">
         <div className="w-full">
           <div>
